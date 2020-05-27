a077315d1ba0ebe9cbfccf1f108815471d546b6f
sieraAlex <fa1nt.siera@gmail.com>
2020-05-27 13:55:37 +0300

    add new project

---
+++ react-app-ts-train/src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center',
+    };
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
